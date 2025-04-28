b61e3e0e38846818267ac6b0b7177e428d777a69
majiale <2281525307@qq.com>
2025-04-28 22:02:00 +0800

    适配hash-cache

--- src/main/java/io/github/pangju666/test/model/entity/TestDO.java
+++ src/main/java/io/github/pangju666/test/model/entity/TestDO.java
@@ -2,8 +2,19 @@ package io.github.pangju666.test.model.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.github.pangju666.framework.data.mybatisplus.model.entity.auto.AutoIdLogicBasicDO;
+import jakarta.annotation.Nullable;
+import lombok.*;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @TableName("test")
 public class TestDO extends AutoIdLogicBasicDO {
 	private String label;
+
+	public TestDO() {
+	}
+
+	public TestDO(String label) {
+		this.label = label;
+	}
 }
--- src/test/groovy/io/github/pangju666/test/TestRepositorySpec.groovy
+++ src/test/groovy/io/github/pangju666/test/TestRepositorySpec.groovy
@@ -1,6 +1,8 @@
 package io.github.pangju666.test
 
+import io.github.pangju666.test.model.entity.TestDO
 import io.github.pangju666.test.repository.TestRepository
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootContextLoader
 import org.springframework.test.context.ContextConfiguration
@@ -11,13 +13,41 @@ class TestRepositorySpec extends Specification {
 	@Autowired
 	TestRepository repository
 
-	def "测试"() {
+	def "测试逻辑删除"() {
 		setup:
 		repository.removeById(1L)
 	}
 
-	def "测试2"() {
+	def "添加随机数据"() {
 		setup:
-		repository.list()
+		List<TestDO> testDOList = new ArrayList<>(10000)
+		for (i in 0..<10000) {
+			testDOList.add(new TestDO(RandomStringUtils.insecure().nextAlphabetic(10)))
+		}
+		repository.saveBatch(testDOList)
+	}
+
+	def "测试查询全部数据"() {
+		when:
+		def list = repository.list()
+
+		then:
+		list.size() == 10000
+	}
+
+	def "测试查询单条数据"() {
+		when:
+		def testDO = repository.getById(10003L)
+
+		then:
+		testDO.getLabel() == "mAmhnTwfHt"
+	}
+
+	def "测试查询多条数据"() {
+		when:
+		def testDO = repository.listByIds(List.of(10003L, 10021L, 10020L))
+
+		then:
+		testDO.size() == 3
 	}
 }
