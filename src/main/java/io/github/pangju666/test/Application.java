68d5cb232aeb01349e4e8b1b8edc8709f28e9f1d
胖橘 <nullptr255@outlook.com>
2025-10-22 04:08:31 +0800

    暂存

--- src/main/java/io/github/pangju666/test/Application.java
+++ src/main/java/io/github/pangju666/test/Application.java
@@ -1,10 +1,10 @@
 package io.github.pangju666.test;
 
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation//.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("io.github.pangju666.test.mapper")
+//@MapperScan("io.github.pangju666.test.mapper")
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
--- src/main/java/io/github/pangju666/test/config/BeanConfig.java
+++
@@ -1,7 +0,0 @@
-package io.github.pangju666.test.config;
-
-import org.springframework.boot.SpringBootConfiguration;
-
-@SpringBootConfiguration
-public class BeanConfig {
-}
